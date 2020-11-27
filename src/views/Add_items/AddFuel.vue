b86ebf950e70086c3c55030b5c60fa4d905485ff
Jaka Rizmal <jrizmal@gmail.com>
2020-11-27 22:06:47 +0100

    test forme

--- src/views/Add_items/AddFuel.vue
+++ src/views/Add_items/AddFuel.vue
@@ -1,15 +1,38 @@
 <template>
-   <page-container title="Tankanje">
-
+  <page-container title="Tankanje">
+    <div class="input-container md-layout-item md-size-100">
+      <md-field>
+        <label>Količina (litri)</label>
+        <md-input type="number" v-model="amount"></md-input>
+      </md-field>
+    </div>
+    
+    <div class="input-container md-layout-item md-size-100">
+      <md-field>
+        <label>Kilometri na števcu</label>
+        <md-input type="number" v-model="kilometers"></md-input>
+      </md-field>
+    </div>
+    <md-button class="md-raised md-primary">Primary</md-button>
   </page-container>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      kilometers: 0,
+      amount: 0,
+    };
+  },
+};
 </script>
 
 <style>
-
+.input-container {
+  background-color: white;
+  border-radius: 20px;
+  padding: 5px 15px 0px 15px;
+  margin-bottom: 10px;
+}
 </style>
